4f4c1e44ad309a5cc6e1bd2ccf0c1333eb0f6d3f
Guillermo <you@example.com>
2018-05-30 12:02:55 +0200

    Added hbs utils and install bower

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let hbs = require("hbs");
+let hbsUtils = require("hbs-utils")(hbs);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//view engine partials
+hbsUtils.registerPartials(`${__dirname}/views/partials`);
+hbsUtils.registerWatchedPartials(`${__dirname}/views/partials`);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
