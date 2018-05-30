f8e6fd97d15dcce52e2f5cad0c0b3764c830847c
Guillermo <you@example.com>
2018-05-30 10:38:39 +0200

    Actualice gulpfile and add concurrently

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,56 @@
 let gulp = require("gulp"),
-browserSync = require("browser-sync"),
+//browserSync = require("browser-sync"),
 uglify = require("gulp-uglify"),
 concat = require("gulp-concat"),
 rename = require("gulp-rename"),
 minify = require("gulp-clean-css");
 
-gulp.task("default", ["browser","scripts","styles"]);
+gulp.task("default", ["scripts","styles"]);
 
-gulp.task("browser", () => {
-    browserSync.init({
-        server:{
-            baseDir: "./"
-        }
-    });
-});
+// gulp.task("browser", () => {
+//     browserSync.init({
+//         server:{
+//             baseDir: "./"
+//         }
+//     });
+// });
 
 gulp.task("scripts",()=>{
     return gulp.src("public/javascripts/**/*.js")
     .pipe(concat("script.js"))
-    .pipe(gulp.dest("dist/script"))
+    .pipe(gulp.dest("public/dist/script"))
     .pipe(rename("script.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest("dist/script"));
+    .pipe(gulp.dest("public/dist/script"));
 })
 
 gulp.watch("public/javascripts/**/*.js").on("change",()=>{
     return gulp.src("public/javascripts/**/*.js")
     .pipe(concat("script.js"))
-    .pipe(gulp.dest("dist/script"))
+    .pipe(gulp.dest("public/dist/script"))
     .pipe(rename("script.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest("dist/script"));
+    .pipe(gulp.dest("public/dist/script"));
 })
 
 gulp.task("styles",()=>{
     return gulp.src("public/stylesheets/**/*.css")
     .pipe(concat("style.css"))
-    .pipe(gulp.dest("dist/css"))
+    .pipe(gulp.dest("public/dist/css"))
     .pipe(rename("style.min.css"))
     .pipe(minify())
-    .pipe(gulp.dest("dist/css"));
+    .pipe(gulp.dest("public/dist/css"));
 })
 
 gulp.watch("public/stylesheets/**/*.css").on("change",()=>{
     return gulp.src("public/stylesheets/**/*.css")
     .pipe(concat("style.css"))
-    .pipe(gulp.dest("dist/css"))
+    .pipe(gulp.dest("public/dist/css"))
     .pipe(rename("style.min.css"))
     .pipe(minify())
-    .pipe(gulp.dest("dist/css"));
+    .pipe(gulp.dest("public/dist/css"));
 })
 
-gulp.watch(["index.html","./public/stylesheets/style.css"]).on("change",()=>{
-    browserSync.reload();
-});
+// gulp.watch(["index.html","./public/stylesheets/style.css"]).on("change",()=>{
+//     browserSync.reload();
+// });
