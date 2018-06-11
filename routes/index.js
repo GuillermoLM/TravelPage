10bce007916ed6e15492d2fc5d999748b5722454
Guillermo <you@example.com>
2018-06-11 16:19:56 +0200

    Added regiter view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Page' });
 });
 
+router.get('/register', (req,res,next)=>{
+  res.render('register',{
+    title:'Register',
+    layout:'layout'
+  })
+ })
+
+ router.get('/login', (req,res,next)=>{
+  res.render('login',{
+    title:'Login',
+    layout:'layout'
+  })
+ })
+
 module.exports = router;
